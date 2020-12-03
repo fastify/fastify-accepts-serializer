7a40140de61145d0a9cee682f6064103d0a5f99e
Simone Busoli <simone.busoli@gmail.com>
2020-12-03 10:00:10 +0100

    test: 100% coverage (#29)
    Fixes #18

--- SerializerManager.js
+++ SerializerManager.js
@@ -14,20 +14,15 @@ class Serializer {
 class SerializerManager {
   constructor (configuration) {
     this.serializers = configuration.serializers
-    this.cache = {}
   }
 
   findSerializer (types) {
-    const cacheValue = this.cache[types]
-    if (cacheValue) return cacheValue
-
     for (let i = 0; i < types.length; i++) {
       const type = types[i]
 
       for (let j = 0; j < this.serializers.length; j++) {
         const serializer = this.serializers[j]
         if (serializer.isAble(type)) {
-          this.cache[types] = { serializer, type }
           return { serializer, type }
         }
       }
@@ -41,13 +36,11 @@ class SerializerManager {
 }
 
 SerializerManager.build = function (options) {
-  options = options || {}
   options.serializers = options.serializers || []
   return SerializerManager.expand(options, { serializers: [] })
 }
 
 SerializerManager.expand = function (options, fallbackSerializer) {
-  options = options || {}
   options.serializers = options.serializers || []
 
   const serializers = options.serializers.map(c => new Serializer(c))
